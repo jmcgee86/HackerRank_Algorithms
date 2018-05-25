d6797ac08952ffc3a49256e140cc065220eac8ad
Jim McGee <jmcgee86@gmail.com>
2018-05-25 18:19:15 +0000

    add solution for Sock Merchant

---
+++ SockMerchant.cs
@@ -0,0 +1,51 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+    var counter = 0;
+    var matches = 0;
+Array.Sort(ar);
+    for (var i=0; i<n-1; i+=counter)
+    {   
+        counter=0;
+        for (var j = i; j<n; j++)
+        {    if(ar[i]==ar[j])
+                counter++;
+        }
+        matches+=counter/2;
+    }
+        
+return matches;
+
+}
+
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
