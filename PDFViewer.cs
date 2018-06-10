22f1178458260aa768fd4a0daca9c15ba0644c18
Jim McGee <jmcgee86@gmail.com>
2018-06-10 21:46:59 +0000

    add solution for Design and PDF Viewer

---
+++ PDFViewer.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the designerPdfViewer function below.
+    static int designerPdfViewer(int[] h, string word) {
+        var highest=0;
+        var l = word.Length;
+        for(var i=0; i<l; i++)
+        {
+          if(highest<h[word[i]-97])
+          {
+            highest = h[word[i]-97];
+          }
+      }
+return highest*l;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp))
+        ;
+        string word = Console.ReadLine();
+
+        int result = designerPdfViewer(h, word);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
