d4154fc3ec0a36f699d7ca71c93fbbc36ba5bcc8
Jim McGee <jmcgee86@gmail.com>
2018-06-12 18:21:44 +0000

    add solution for Viral Advertising

---
+++ ViralAdvertising.cs
@@ -0,0 +1,44 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        var recipients = 5;
+        var likes = 0;
+        for(var i = 1; i<=n; i++)
+        {
+            var dayLikes = recipients/2;
+            likes += dayLikes;
+            recipients = dayLikes * 3;
+        }
+        
+        return likes;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = viralAdvertising(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
