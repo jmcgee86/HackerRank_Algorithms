b3c16c111fc91234139ba989d236c363c5156494
Jim McGee <jmcgee86@gmail.com>
2018-06-15 17:40:19 +0000

    add solution for Find Digits

---
+++ FindDigits.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        var divisor =0;
+        var nums = n.ToString().ToCharArray();
+        foreach (var digit in nums)
+        {
+            if (digit!='0' && n%int.Parse(digit.ToString())== 0)
+                divisor++;
+            
+        }
+                 
+    return divisor;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = findDigits(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
