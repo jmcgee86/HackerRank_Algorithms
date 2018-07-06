ed8c18435934fdc8266113722a3895b8ac14ae53
Jim McGee <jmcgee86@gmail.com>
2018-07-06 17:23:18 +0000

    add solution for Minimum Distances

---
+++ MinimumDistances.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumDistances function below.
+    static int minimumDistances(int[] a) {
+        var distance = -1;
+        var tempDistance = a.Length+1;
+        for (var i =0; i<a.Length-1; i++)
+        {
+            for(var j=i+1; j<a.Length; j++)
+            {
+                if(a[i]==a[j] && Math.Abs(i-j)<tempDistance)
+                    tempDistance = Math.Abs(i-j);
+            }
+            if(tempDistance < a.Length+1 )
+                distance = tempDistance;
+        }
+
+        return distance;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        int result = minimumDistances(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
