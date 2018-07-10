dde088f0e18834bd29b1508a4304be07303763a8
Jim McGee <jmcgee86@gmail.com>
2018-07-10 14:55:10 +0000

    add solution for Jumping on the Clouds

---
+++ JumpingOnTheCloudsImplementation.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+        var position = 0;
+        var jumps =0;
+        while (position != c.Length-1)
+            if(position +2 <= c.Length-1 && c[position+2]==0)
+            {
+                position +=2;
+                jumps ++;
+            }
+            else
+            {
+                position +=1;
+                jumps++;
+            }
+        return jumps;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = jumpingOnClouds(c);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
